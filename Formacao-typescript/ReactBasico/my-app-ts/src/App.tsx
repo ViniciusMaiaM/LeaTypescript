4a46bb4845f626f03c4cd13c998d83eba1efbd84
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-01-06 10:26:08 -0300

    feat: add footer e header

--- Formacao-typescript/ReactBasico/my-app-ts/src/App.tsx
+++ Formacao-typescript/ReactBasico/my-app-ts/src/App.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Card } from './components/Card';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <>
+      <Header/>
       <div>
         Hello World!
-        <Card id={1}/> {/*É necessário passar o parâmetro dessa maneira */} 
-        <Card id={2}/>
+        <Card 
+          id={1}  /*É necessário passar o parâmetro dessa maneira */
+          paragraph='TypeScript'
+          details='Ts para frontend e backend'
+        /> 
+        <Card 
+          id={2}
+          paragraph='HTML'
+          details='HTML para frontend'
+          />
       </div>
-      <div>
-        footer
-      </div>
+      <Footer/>
     </>
   );
 }
