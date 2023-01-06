339d0ef3f431b955d63dea1c895535b4dfe59596
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-01-06 10:00:06 -0300

    chore: add interface e passando id de card

--- Formacao-typescript/ReactBasico/my-app-ts/src/components/Card.tsx
+++ Formacao-typescript/ReactBasico/my-app-ts/src/components/Card.tsx
@@ -1,7 +1,12 @@
-export const Card = () => {
+interface ICard {
+    id: number
+}
+
+export const Card = ({id} : ICard) => {
+    console.log(id)
     return(
         <div>
-            <h1>Card</h1>
+            <h1>Card {id}</h1>
             <p>Paragrafos</p>
             <p>Details about card</p>
         </div>
