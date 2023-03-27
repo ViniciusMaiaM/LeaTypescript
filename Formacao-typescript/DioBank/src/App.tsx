e5d6244a8efbac7a71f1e86701a19b288219f3a6
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-27 10:47:38 -0300

    Update app and routes

--- Formacao-typescript/DioBank/src/App.tsx
+++ Formacao-typescript/DioBank/src/App.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
 import { 
     ChakraProvider,
 } from '@chakra-ui/react'
-
-import { Card } from './components/Card';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Conta from './components/pages/Conta';
+import { Layout } from './components/Layout';
 function App() {
   // Value é o valor e setValue a função para alterar este valor
     const [email, setEmail] = useState('');
     return (
-        <ChakraProvider>
-            <Card />
-        </ChakraProvider>
+        <BrowserRouter>
+            <ChakraProvider>
+                <Layout>
+                    <Routes>
+                        <Route path='/'element={
+                            <Home/>
+                        }/>
+                        <Route path='/conta'element={
+                            <Conta/>
+                        }/>
+                    </Routes>
+                </Layout>
+            </ChakraProvider>
+        </BrowserRouter>
     );
 }
 
