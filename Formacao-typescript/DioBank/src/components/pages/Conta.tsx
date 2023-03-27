25dc04199f2a4b96709da36203b2ae3b44302848
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-27 11:52:36 -0300

    Add useEffect and userData

--- Formacao-typescript/DioBank/src/components/pages/Conta.tsx
+++ Formacao-typescript/DioBank/src/components/pages/Conta.tsx
@@ -1,6 +1,33 @@
+import { Box, Center, SimpleGrid } from '@chakra-ui/react'
+import CardInfo from '../CardInfo'
+import { useEffect, useState} from 'react';
+import { api } from '../api';
+
+interface userData{
+    email: string,
+    password: string,
+    name: string,
+    balance: number
+}
+
 const Conta = () => {
+    const [userData, setUserData] = useState<null | userData>();
+
+    useEffect(() => {
+        const getData = async () => {
+            const data: any | userData = await api;
+            setUserData(data);
+        }
+
+        getData();
+    }, [])
     return (
-        <h1>Conta</h1>
+        <Center>
+            <SimpleGrid columns={2} spacing={8} paddingTop={16}>
+                <CardInfo text='Informações de acesso'/>
+                <CardInfo text='Informações da conta'/>
+            </SimpleGrid>
+        </Center>
     )
 }
 
